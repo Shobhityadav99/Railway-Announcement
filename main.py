14595ca1524de9e8b91875d0415f773707c6c24b
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-10 14:42:06 +0530

    Basic func added

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import os
 import pandas as pd
 from pydub import AudioSegment
 from gtts import gTTS
+
+def textToSpeech(text,fileName):
+    pass
+
+def mergeAudios(audios):
+    pass
+
+def generateSkeleton():
+    pass
+
+def generateAnnouncement(fileName):
+    pass
+
+if __name__ == "__main__":
+    pass
