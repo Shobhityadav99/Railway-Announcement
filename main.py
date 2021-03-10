f6eda0bd8cd33d8ddccb0f2eda451fd4e13273d9
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-10 15:09:05 +0530

    1st audio generated

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ def mergeAudios(audios):
     pass
 
 def generateSkeleton():
-    pass
+    audio = AudioSegment.from_mp3('railway.mp3')
+    start = 88000
+    finish = 90200
+    audioprocessed = audio[start:finish]
+    audioprocessed.export("1_hindi.mp3",format="mp3")
+
+
 
 def generateAnnouncement(fileName):
     pass
 
 if __name__ == "__main__":
-    pass
+    generateSkeleton()
